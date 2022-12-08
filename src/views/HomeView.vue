58e944f5edd083c813705fb7ea981cbfe82da9fe
Tristan <tristanthorburn@gmail.com>
2022-12-07 20:01:05 -0500

    Api returning dynamic data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,24 @@
+<script setup>
+  import axios from 'axios';
+  import { ref } from 'vue';
+
+  const user = ref('');
+
+  const fetchData = () => {
+    console.log(user.value)
+    axios
+      .get(`https://api.github.com/users/${user.value}`)
+      .then(response => console.log(response))
+  }
+
+</script>
+
 <template>
   <main>
-    <h1>Landing Page</h1>
+    <h1>Test</h1>
+  
+    <input type="text" v-model="user"/>
+    <button type="submit" @click="fetchData">Submit</button>
+    <p>the user searched is {{ user }}</p>
   </main>
 </template>
